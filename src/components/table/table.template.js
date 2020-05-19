cae06d1265a1aa04ad97e71f709d3749b37d29e4
roden1978 <rdn.homebox@gmail.com>
2020-05-19 14:32:15 +0500

    finish base framework functional (#4)

---
+++ src/components/table/table.template.js
@@ -0,0 +1,44 @@
+const CODES = {
+    A: 65,
+    Z: 90
+}
+
+function toCell() {
+    return `<div class="cell" contenteditable></div>`
+}
+
+function createRow(content, index) {
+    return `
+ <div class="row">
+    <div class="row-info">${index ? index : ''}</div>
+    <div class="row-data">${content}</div>
+ </div>`
+}
+
+function toCol(col) {
+    return `<div class="column">${col}</div>`
+}
+
+function toChar(_, index) {
+    return String.fromCharCode(CODES.A + index)
+}
+
+export function createTable(rowsCount = 25) {
+    const colsCount = CODES.Z - CODES.A + 1
+    const rows = []
+    const cols = new Array(colsCount)
+        .fill('')
+        .map(toChar)
+        .map(toCol)
+        .join('')
+    rows.push(createRow(cols, null))
+
+    for (let i = 0; i < rowsCount; i++) {
+        const cells = new Array(colsCount)
+            .fill('')
+            .map(toCell)
+            .join('')
+        rows.push(createRow(cells, i + 1))
+    }
+    return rows.join('')
+}
